ce0bac237c218c12fe191fc15d09b2d5395894fc
Soufiane ELBAZ <elbaz@mobiblanc.com>
2019-02-17 11:18:16 +0100

    Bug fixes and some code cleaning.

--- google_auth.js
+++ google_auth.js
@@ -10,37 +10,38 @@ class GoogleAuthentication {
 
     signin() {
         (async () => {
+            // await this.page.screenshot({ path: 'google_auth.png' });
             // Waiting for email form elements
-            await this.page.waitForSelector(`#identifierId`);
-            await this.page.waitForSelector(`#identifierNext`);
+            await this.page.waitForSelector(`#Email`);
+            await this.page.waitForSelector(`#next`);
 
             console.log(`Typing email ...`);
 
             // Typing email
-            await this.page.type(`#identifierId`, this.email);
+            await this.page.type(`#Email`, this.email);
 
             // Going to the next page
-            await this.page.click('#identifierNext');
+            await this.page.click('#next');
 
             // Waiting for the transition to be finished
             await this.page.waitFor(2000);
 
             // Waiting for password form elements
-            await this.page.waitForSelector(`#password > div > div > div > input`);
-            await this.page.waitForSelector('#passwordNext');
+            await this.page.waitForSelector(`#Passwd`);
+            await this.page.waitForSelector('#signIn');
 
             console.log(`Typing password ...`);
 
             // Typing password
-            await this.page.type(`#password > div > div > div > input`, this.password);
+            await this.page.type(`#Passwd`, this.password);
 
             // Waiting for the password to be filled in
-            await this.page.waitForSelector(`#password > div > div > div > input`, { value: this.password });
+            await this.page.waitForSelector(`#Passwd`, { value: this.password });
 
             console.log(`Now clicking on button next`);
 
             // Validating our credentials
-            await this.page.click('#passwordNext');
+            await this.page.click('#signIn');
 
             // Waiting for the transition to be finished
             await this.page.waitForNavigation();
